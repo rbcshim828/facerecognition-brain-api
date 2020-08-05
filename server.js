a460c1261d76b9948864f961090ae609b112403a
Rebecca Shim <rbcshim828@gmail.com>
2020-08-05 17:21:26 +0900

    completed project

---
+++ server.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
+  ]
+}
+
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+// signin
+app.post('/signin', (req, res) => {
+  if(req.body.email === database.users[0].email &
+     req.body.password === database.users[0].password){
+       res.json(database.users[0]);
+     } else {
+       res.status(400).json('error loging in');
+     }
+})
+
+// register
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+     console.log(hash);
+  });
+
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  console.log(database.users.length);
+  res.json(database.users[database.users.length-1]);
+})
+
+// profile
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if(!found){
+    res.status(400).json('not found');
+  }
+});
+
+// image
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if(!found){
+    res.status(400).json('not found');
+  }
+})
+
+//bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+//});
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+})
+/*
+/ --> rs = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
